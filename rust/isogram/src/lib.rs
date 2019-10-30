7a0a96d47fed77ea794efa4f8253b29fb0e012e1
Joe Roland <joroland@gmail.com>
2019-10-30 18:47:51 -0400

    Forgot to save

--- rust/isogram/src/lib.rs
+++ rust/isogram/src/lib.rs
@@ -2,16 +2,6 @@ use std::collections::HashSet;
 
 pub fn check(candidate: &str) -> bool {
   let mut seen: HashSet<char> = HashSet::new();
-  // for c in candidate.to_lowercase().chars() {
-  //   if !c.is_alphabetic() {
-  //     continue;
-  //   }
-  //   if seen.contains(&c) {
-  //     return false;
-  //   }
-  //   seen.insert(c);
-  // }
-  // true
   candidate.to_lowercase().chars().all(|c| {
     if !c.is_alphabetic() {
       true
