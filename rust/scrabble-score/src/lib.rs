dec2c7d7b4b445ef162a90d2af890ce4cbeb1753
Joe Roland <joroland@gmail.com>
2019-11-03 21:50:59 -0500

    Added Rust scrabble-score solution

---
+++ rust/scrabble-score/src/lib.rs
@@ -0,0 +1,17 @@
+/// Compute the Scrabble score for a word.
+pub fn score(word: &str) -> u64 {
+  word.to_ascii_uppercase().chars().fold(0, |mut acc, c| {
+    acc += match c {
+      'A' | 'E' | 'I' | 'O' | 'U' | 'L' | 'N' | 'R' | 'S' | 'T' => 1,
+      'D' | 'G' => 2,
+      'B' | 'C' | 'M' | 'P' => 3,
+      'F' | 'H' | 'V' | 'W' | 'Y' => 4,
+      'K' => 5,
+      'J' | 'X' => 8,
+      'Q' | 'Z' => 10,
+      _ => 0,
+    };
+
+    acc
+  })
+}
